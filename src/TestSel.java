7c7ec052cb67eb2afaf830b16032a35fb722d1a3
student <studentstups@gmail.com>
2017-03-29 20:29:02 +0300

    v

--- src/TestSel.java
+++ src/TestSel.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
@@ -10,25 +12,45 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestSel {
 
-    public static void main(String[] args){
-        System.setProperty("webdriver.gecko.driver","C:\\Users\\student.STEP\\Downloads\\geckodriver-v0.15.0-win64 (2)//geckodriver.exe");
+    public static void main(String[] args) throws InterruptedException{
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\student.STEP\\Selenium\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.amazon.com/");
+        WebElement newAccountElement = driver.findElement(By.id("nav-flyout-ya-newCust"));
+        WebElement newAccountLinkElement = newAccountElement.findElement(By.tagName("a"));
+        String newAccountLink = newAccountLinkElement.getAttribute("href");
+        driver.get(newAccountLink);
+
+        WebElement inputNameField = driver.findElement(By.id("ap_customer_name"));
+        inputNameField.sendKeys("AibAibaaaaa");
+
+        WebElement inputEmailField = driver.findElement(By.id("ap_email"));
+        inputEmailField.sendKeys("asdfgjJJWlsM@gmail.com");
+
+        WebElement inputPassField = driver.findElement(By.id("ap_password"));
+        inputPassField.sendKeys("Papspdasdk");
+
+        WebElement inputPassCheckField = driver.findElement(By.id("ap_password_check"));
+        inputPassCheckField.sendKeys("Papspdasdk");
+
+        try {
+            Thread.sleep(1000*20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement regBtn = driver.findElement(By.id("continue"));
+        regBtn.click();
+
+        String logginedPageLink = driver.getCurrentUrl();
+
+        
 
 
-        ProfilesIni profiles = new ProfilesIni();
 
+        driver.quit();
 
-
-
-
-        WebDriver webdr = new FirefoxDriver();
-        webdr.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-        webdr.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
-
-        webdr.get("https://www.ukr.net");
-
-
-
-        webdr.quit();
     }
 
 }
